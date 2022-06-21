f3a6780b71b939336c7078513fdce57996762146
Lakeram Narine <nar19002@byui.edu>
2022-06-21 08:36:55 -0600

    Added swagger and dependency

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Commands API',
+    description: 'This will commands for various lanaguages and Frameworks'
+  },
+  host: 'week5projectlake.herokuapp.com',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./Routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
