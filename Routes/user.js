3003c5d68209942e1be3a0914e06e1dfd3c0f085
smith-tanner22 <smith.tanner2222@gmail.com>
2022-06-29 08:41:13 -0600

    addded controller stuff and routes

---
+++ Routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/user');
+
+// const validation = require('../middleware/validate');
+
+router.get('/', userController.getAll);
+
+router.get('/:id', userController.getSingle);
+
+router.post('/', validation.saveUser, userController.createUser);
+
+router.put('/:id', validation.saveUser, userController.updateUser);
+
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
