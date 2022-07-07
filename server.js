2d28bb2f0269e945d7c4e878cbbd6565e0bdabff
smith-tanner22 <smith.tanner2222@gmail.com>
2022-07-06 18:49:43 -0600

    added some stuff for auth0

--- server.js
+++ server.js
@@ -5,11 +5,30 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const bodyParser = require('body-parser');
 const app = express();
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
 // test 3
 
 var options = {
   explorer: true,
 };
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 // app
 //   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
