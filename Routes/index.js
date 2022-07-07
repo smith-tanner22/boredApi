29df1199c4d109767e04ae9edc88be14b7e4af7d
smith-tanner22 <smith.tanner2222@gmail.com>
2022-07-06 19:31:14 -0600

    added authenticaiton stuff

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const { requiresAuth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/ideas', require('./ideas'))
-router.use('/medias', require('./media'))
-router.use('/reviews', require('./reviews'))
-router.use('/users', require('./user'))
+router.use('/ideas', require('./ideas'));
+router.use('/medias', require('./media'));
+router.use('/reviews', require('./reviews'));
+router.use('/users', requiresAuth(), require('./user'));
 
 module.exports = router;
