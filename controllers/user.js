acaee9b11d19215f7201ea80efc773a2c0393041
smith-tanner22 <smith.tanner2222@gmail.com>
2022-07-13 19:01:04 -0600

    fixed some formatting issues

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,28 @@
 const mongodb = require('../DataBase/dbConnect');
 const ObjectId = require('mongodb').ObjectId;
-const {CreateUserBodyValid} = require("../Utils/validation")
-
+const { CreateUserBodyValid } = require('../Utils/validation');
 
 const getAll = async (req, res) => {
-try{
-  mongodb
-    .getDb()
-    .db()
-    .collection('users')
-    .find()
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-
- } catch (error) {
+  try {
+    mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
+  } catch (error) {
     res.status(500).json(error);
   }
 };
 
 const getSingle = async (req, res) => {
-
-try{
+  try {
     if (!ObjectId.isValid(req.params.id)) {
       res.status(400).json('Must you a valid user id to find a user.');
     }
@@ -42,79 +39,77 @@ try{
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(result[0]);
       });
-   } catch (error) {
+  } catch (error) {
     res.status(500).json(error);
   }
 };
 
 const updateUser = async (req, res) => {
-try{
-      if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid user id to update a user.');
-      }
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid user id to update a user.');
+    }
 
-      const {error} = await CreateUserBodyValid(req.body)
-      if(error)
-      {
-        console.log(error)
-        return res.status(400).json(error.message)
-      }
+    const { error } = await CreateUserBodyValid(req.body);
+    if (error) {
+      console.log(error);
+      return res.status(400).json(error.message);
+    }
 
-      const userId = new ObjectId(req.params.id);
-      const user = {
-        email: req.body.email,
-        password: req.body.password,
-      };
-      const response = await mongodb
-        .getDb()
-        .db()
-        .collection('users')
-        .replaceOne({ _id: userId }, user);
-      console.log(response);
-      if (response.modifiedCount > 0) {
-        res.status(204).json(response);
-      } else {
-        res
-          .status(500)
-          .json(response.error || 'Some error occuring while updating the user.');
-      }
- } catch (error) {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .replaceOne({ _id: userId }, user);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).json(response);
+    } else {
+      res
+        .status(500)
+        .json(response.error || 'Some error occuring while updating the user.');
+    }
+  } catch (error) {
     res.status(500).json(error);
   }
 };
 
 const createUser = async (req, res) => {
-try{
-      const {error} = await CreateUserBodyValid(req.body)
-      if(error)
-      {
-        console.log(error)
-        return res.status(400).json(error.message)
-      }
-      
-      const user = {
-        email: req.body.email,
-        password: req.body.password,
-      };
-      const response = await mongodb
-        .getDb()
-        .db()
-        .collection('users')
-        .insertOne(user);
-      if (response.acknowledged) {
-        res.status(201).json(response);
-      } else {
-        res
-          .status(500)
-          .json(response.error || 'Some error occuring while creating the user.');
-      }
- } catch (error) {
+  try {
+    const { error } = await CreateUserBodyValid(req.body);
+    if (error) {
+      console.log(error);
+      return res.status(400).json(error.message);
+    }
+
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .insertOne(user);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res
+        .status(500)
+        .json(response.error || 'Some error occuring while creating the user.');
+    }
+  } catch (error) {
     res.status(500).json(error);
   }
 };
 
 const deleteUser = async (req, res) => {
-try{
+  try {
     if (!ObjectId.isValid(req.params.id)) {
       res.status(400).json('Must use a valid user id to delete a user.');
     }
@@ -132,7 +127,7 @@ try{
         .status(500)
         .json(response.error || 'Some error occured while deleting the user.');
     }
- } catch (error) {
+  } catch (error) {
     res.status(500).json(error);
   }
 };
